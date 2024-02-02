2dd7ef5d364ed0caf999b18ee1e71f7c5b3ed01c
Nathnael Teshome <nathnaelteshome10@gmail.com>
2024-02-02 13:04:11 +0300

    added

--- 1.py
+++ 1.py
@@ -1,8 +1,13 @@
-title = "Smallest Even Multiple"
+title = "Mod Power"
 
 arr = title.split(" ")
 res = ""
 for word in arr:
     res += word.lower() + "_"
+    name = res[:-1] + ".py"
 
 print(res[:-1] + ".py")
+
+file = open(name , 'w')
+file.close()
+
