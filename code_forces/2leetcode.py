8a24bf8628ad4ad4e296db20d86a3d596082c215
Nathnael Teshome <nathnaelteshome10@gmail.com>
2024-05-10 23:13:45 +0300

    Update Solution class to implement continuousSubarrays method for counting continuous subarrays

--- code_forces/2leetcode.py
+++ code_forces/2leetcode.py
@@ -1,19 +1,30 @@
-
 from collections import defaultdict, deque
 from typing import List, Optional
 
 
 class Solution:
-    def myPow(self, x: float, n: int) -> float:
-        # if n is negative:
-        if n == 0:
-            return 1
-        if n < 0:
-            n = abs(n)
-            negative = True  
-        else: 
-            negative = False
-        return self.myPow(x, abs(n - 1)) * x if not negative else 1 / (self.myPow(x, abs(n - 1)) * x)
+    def continuousSubarrays(self, nums: List[int]) -> int:
+        incr = deque()  # increasing monotonic queue
+        dec = deque()  # decreasing monotonic queue
+        l = 0
+        ans = 0
+
+
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+        cur = 0
+        left, right = 1, 2 ** (n - 1)
+
+        for _ in range(n - 1):
+            mid = (left + right) // 2
+            if k <= mid:
+                right = mid
+            else:
+                left = mid + 1
+                cur = 0 if cur == 1 else 1
+
+        return cur
+
 
 soln = Solution()
-
+print(soln.kthGrammar(4, 5))  # 1
