665926267a090c36989328dd57f589483bf51c88
Nathnael Teshome <nathnaelteshome10@gmail.com>
2024-06-26 15:27:52 +0300

    djsk

--- code_forces/4leetcodeBacktracking.py
+++ code_forces/4leetcodeBacktracking.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from typing import Optional
 
 
@@ -11,27 +12,33 @@ class TreeNode:
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
         path = 0
+        prefix_sum = defaultdict(int)
+        prefix_sum[0] = 1
 
-        def backtrack(node, prev):
+        def backtrack(node, prefix_sum, prev):
+            nonlocal path  # Declare path as a nonlocal variable
             if not node:
                 return
 
-            value = node.val
-            if value - targetSum in prev:
-                path += 1
+            value = node.val + prev[-1]
+            if value - targetSum in prefix_sum:
+                path += prefix_sum[value - targetSum]
 
+            prev.append(value)
+            prefix_sum[value] += 1
             if node.left:
-                prev.append(node.left)
-                backtrack(node.left, prev)
-                prev.pop()
+                backtrack(node.left, prefix_sum, prev.copy())
+                prefix_sum[prev[-1]] -= 1
 
             if node.right:
-                prev.append(node.right)
-                backtrack(node.right, prev)
+                backtrack(node.right, prefix_sum, prev.copy())
+                prefix_sum[prev[-1]] -= 1
 
-        backtrack(root, [0])
+        backtrack(root, prefix_sum, [0])
         return path
 
+
+# [fun(10, [0]), fun(10, [0, 10]), fun(15, [0, 10]), fun(18, [0,10,15])]
 
 soln = Solution()
 root = TreeNode(10)
