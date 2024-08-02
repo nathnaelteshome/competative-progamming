4b48a85f57f98aede9c7822eaae1a176c315c55d
Nathnael Teshome <nathnaelteshome10@gmail.com>
2024-08-03 01:49:20 +0300

    Refactor `loudAndRich` function for better efficiency and readability

--- code_forces/leetcode/851. Loud and Rich.py
+++ code_forces/leetcode/851. Loud and Rich.py
@@ -1,27 +1,42 @@
-def loudAndRich(self, richer: List[List[int]], quiet: List[int]) -> List[int]:
-    ans = [[float("inf"), 0]] * len(quiet)
-    adj = collections.defaultdict(list)
-    for a, b in richer:
-        adj[b].append(a)
+import collections
+from typing import List
 
-    def dfs(node):
-        if ans[node][0] != float("inf"):
+
+class Solution:
+    def loudAndRich(self, richer: List[List[int]], quiet: List[int]) -> List[int]:
+        ans = [[0, float("inf")]] * len(quiet)
+        adj_list = collections.defaultdict(list)
+
+        for rich, poor in richer:
+            adj_list[poor].append(rich)
+
+        def dfs(node):
+            if ans[node][1] != float("inf"):
+                return ans[node]
+            ans[node] = [node, quiet[node]]
+
+            for child in adj_list[node]:
+                k = dfs(child)
+                if k[1] < ans[node][1]:
+                    ans[node] = k
+
             return ans[node]
-        ans[node] = [quiet[node], node]
 
-        for rich_Nei in adj[node]:
-            k = dfs(rich_Nei)
-            if k[0] < ans[node][0]:
-                ans[node] = k
+            minimum = min(quiet[node], quiet[child])
 
-        return ans[node]
-
-    for node in range(len(quiet)):
-        if ans[node][0] == float("inf"):
-            dfs(node)
-
-    return [i[1] for i in ans]
+        print(adj_list)
+        for index in range(len(quiet)):
+            if ans[index][1] == float("inf"):
+                dfs(index)
 
 
 # input: richer = [[1,0],[2,1],[3,1],[3,7],[4,3],[5,3],[6,3]], quiet = [3,2,5,4,6,1,7,0]
 # Output: [5,5,2,5,4,5,6,7]
+
+soln = Solution()
+print(
+    soln.loudAndRich(
+        richer=[[1, 0], [2, 1], [3, 1], [3, 7], [4, 3], [5, 3], [6, 3]],
+        quiet=[3, 2, 5, 4, 6, 1, 7, 0],
+    )
+)
