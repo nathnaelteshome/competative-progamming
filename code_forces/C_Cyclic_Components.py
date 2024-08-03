52f6daf292267b28e2e11a01b8920b1bd273ff6d
Nathnael Teshome <nathnaelteshome10@gmail.com>
2024-08-03 21:44:42 +0300

    Refactor `C_Cyclic_Components.py` for better efficiency and readability

--- code_forces/C_Cyclic_Components.py
+++ code_forces/C_Cyclic_Components.py
@@ -1,17 +1,16 @@
 from collections import defaultdict
 
 
-# verices, edges = [int(x) for x in input().split()]
+verices, edges = [int(x) for x in input().split()]
 
 visited = set()
-# adj_list = defaultdict(list)
-# for _ in range(edges):
-#     node1, node2 = [int(x) for x in input().split()]
-#     adj_list[node1].append(node2)
-#     adj_list[node2].append(node1)
+adj_list = defaultdict(list)
+for _ in range(edges):
+    node1, node2 = [int(x) for x in input().split()]
+    adj_list[node1].append(node2)
+    adj_list[node2].append(node1)
 # defaultdict(<class 'list'>, {1: [2], 2: [1], 3: [4, 5], 4: [3, 5], 5: [4, 3]})
 # defaultdict(<class 'list'>, {1: [8, 12], 8: [1], 12: [1], 5: [11, 15], 11: [5, 9], 9: [11, 15], 15: [9, 5], 4: [13, 3, 14], 13: [4, 3], 3: [13, 4, 14], 10: [16, 7], 16: [10, 7], 7: [10, 16], 14: [3, 4], 17: [6], 6: [17]})
-adj_list = {1: [2], 2: [1], 3: [4, 5], 4: [3, 5], 5: [4, 3]}
 
 
 def find_cycle():
@@ -23,11 +22,13 @@ def find_cycle():
         for child in adj_list[node]:
             if child == prev:
                 continue
+            if len(adj_list[child]) != 2:
+                return False
+
             if child in visited:
                 return True
             if dfs(child, node):
                 ans += 1
-                return True
 
     for idx in adj_list.keys():
         if idx not in visited:
